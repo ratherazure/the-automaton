39f2bc0e160a7996af53be9515fc857070a2c615
ratherazure <azurenightlight@gmail.com>
2020-01-26 16:58:53 +1100

    eliminated redundant code

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import re
 import json
 import random
 
+import modules
 from modules import *
 
 class TheClient(discord.Client):
@@ -29,39 +30,24 @@ class TheClient(discord.Client):
 
     async def on_message(self, message):
         response = None
-
-        if message.content.startswith(commandInvoker) and commands.get(message.content.split(" ")[0][len(commandInvoker):], [None, None])[0] != [None, None]: #if message starts with the command handle and is in the command dictionary then...
-            command = commands.get(message.content.split(" ")[0][len(commandInvoker):], [None, None]) #take the invoked command and return with a python pointer to a function
-            if command[1] == []: #for some reason, one line if statements don't work here?
+        if message.content.startswith(commandInvoker):
+            if checkifModuleExists(message.content.split(" ")[0][len(commandInvoker):]):
+                command = message.content.split(" ")[0][len(commandInvoker):]
                 parameters = message.content.split(" ")[1:] #if there are no predetermined parameters, take them from the message itself.
-            else:
-                parameters = command[1]
-            response = command[0](*parameters) #execute the function with the parameters.
+                response = sys.modules["modules.{}".format(message.content.split(" ")[0][len(commandInvoker):])].main(*parameters)
 
-        if response == None: #if the a response has not yet been set then...
-            for entry in interceptResponses: #if any of the triggers exists in any part message, then...
-                if re.search(r"\b{}\b".format(entry), message.content, re.IGNORECASE) != None:
-                    payload = random.choice(interceptResponses[entry])
-                    response = {"content": payload, "file": None, "embed": None} #set the payload
-
-        if response != None: #don't send nothing
+        if response != None:
             await message.channel.send(content = response.get("content", None), embed = response.get("embed", None), file = response.get("file", None))
 
-def checkifImported(moduleinQuestion): #checks if custom module is imported
-    return "modules.{module}".format(moduleinQuestion) in sys.modules.keys()
+def checkifModuleExists(moduleinQuestion): #checks if custom module is imported
+    return "modules.{module}".format(module = moduleinQuestion) in sys.modules.keys()
 
 if __name__ == "__main__":
 
     colorama.init()
     commandInvoker = "/"
 
-    #the place where you map a command to the function, and also where you specify your parameters
-    commands = {
-        "debug": (debug, ["Hello", "World", "!"]),
-        "story": (story, [])
-    }
-
     interceptResponses = json.loads(open(os.path.join("data", "responses.json")).read())
 
-    #client = TheClient()
-    #client.run(open(os.path.join("data", "discordToken.txt")).read())
+    client = TheClient()
+    client.run(open(os.path.join("data", "discordToken.txt")).read())
